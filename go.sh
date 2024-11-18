c7405fb7ed3522027b041cf9a6a8a186ab865a22
Xprerist <xprerist@gmail.com>
2024-11-18 14:25:09 +0100

    Updated go.sh, 18.11.1924

--- go.sh
+++ go.sh
@@ -4,19 +4,23 @@ echo "This is my sript for the UDEMY/CODECLOUD Ansible Training"
 echo "From zero to hero  :-)"
 
 while true; do
-	echo -n "Please enter your name: "
+	# Eingabe des Namens
+	echo -n "Please enter your name (CTRL-C to exit): "
 	read varname
 
+	# Überprüfung des Namens
 	if [[ "$varname" == "Jens" || "$varname" == "Julita" || "$varname" == "Max" ]]; then
   		echo "Hello, $varname!"
-		break	# Exit the loop
+		
+                # Lokale Zeit und Zeitzone holen
+                local_time=$(timedatectl | grep "Local time" | awk '{print $3, $4, $5, $6}')
+                time_zone=$(timedatectl | grep "Time zone" | awk '{print $1, $2, $3}')
+
+                # Ansible Playbook ausführen
+                cd ~/Ansible_from_zero_to-hero 
+                ansible-playbook playground.yml --limit web_server --extra-vars "varname='$varname' local_time='$local_time' time_zone='$time_zone'"
 	else
-  		echo "Please try again"
+  		echo "Invalid name. Please try again"
 	fi
 done
 
-local_time=$(timedatectl | grep "Local time" | awk '{print $3, $4, $5, $6}')
-time_zone=$(timedatectl | grep "Time zone" | awk '{print $1, $2, $3}')
-
-cd ~/Ansible_from_zero_to-hero 
-ansible-playbook playground.yml --limit web_server --extra-vars "varname='$varname' local_time='$local_time' time_zone='$time_zone'"
